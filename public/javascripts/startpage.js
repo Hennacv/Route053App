b0c6acb046932cedf2f5ef62e842dc1b21e9858f
Hennacv <hennaviana6@gmail.com>
2018-07-04 19:13:02 +0200

    added display functions

--- public/javascripts/startpage.js
+++ public/javascripts/startpage.js
@@ -9,9 +9,10 @@ db.ref("masterSheet").once('value').then(function(snapshot){
     locations.push({ name: name, placeId: placeId });
   }
   console.log('locations:', locations);
+
 })
 
-
+console.log('locations:', locations);
 function qrscanner(){
     console.log("clicked");
     window.location.href = "./qrscanner";
@@ -36,6 +37,41 @@ function initMap() {
     // var infowindow = new google.maps.InfoWindow();
     // var service = new google.maps.places.PlacesService(map);
 
+}
+
+function displayAll(){
+    displayStores();
+    displayRestaurants();
+}
+
+function displayStores(){
+    for (var i = 0 ; i < locations.length;  i++) {
+        var placeIdInfo = locations[i][4];
+
+        var infowindow = new google.maps.InfoWindow();
+        var service = new google.maps.places.PlacesService(map);
+    
+        service.getDetails(
+          {
+         placeId: placeIdInfo
+          }, function(place, status) {
+            if (status === google.maps.places.PlacesServiceStatus.OK)
+            {
+           var iconBase = 'http://retroactivesolutions.com/premadeMapForPool2/';
+                 var marker = new google.maps.Marker(
+                {
+                  map: map,
+                  position: place.geometry.location,
+                       icon: iconBase + 'icon-gallery.png'
+                });
+              google.maps.event.addListener(marker, 'click', function()
+                {
+                infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + 'Place ID: ' + place.place_id	+ '<br>' + '<br>'	+ '<b>Adress:</b>' + '<br>'	+ place.formatted_address	+ '</div>'+ '<br>' + 'Opening Times:' + '<br>' + place.opening_hours.weekday_text[0] + '<br>' + place.opening_hours.weekday_text[1]	+ '<br>' + place.opening_hours.weekday_text[2]	+ '<br>' + place.opening_hours.weekday_text[3]	+ '<br>' + place.opening_hours.weekday_text[4] + '<br>' + place.opening_hours.weekday_text[5] + '<br>' + place.opening_hours.weekday_text[6]);
+                infowindow.open(map, this);
+                    });
+                  }
+          });
+    }
 }
 
 function displayRestaurants(){

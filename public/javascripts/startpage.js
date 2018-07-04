1f2f9a8edee55f7403a70a9cdb3936cb07242162
Hennacv <hennaviana6@gmail.com>
2018-07-04 21:11:51 +0200

    displayres

--- public/javascripts/startpage.js
+++ public/javascripts/startpage.js
@@ -1,12 +1,25 @@
-var map;
+var db = require('../initializesdk.js');
+var locations = [];
 
+db.ref("masterSheet").once('value').then(function(snapshot){
+  var allItems = snapshot.val();
+  for(let i = 0; i < allItems.length; i++){
+    var placeId = allItems[i][4];
+    var name = allItems[i][1];
+    locations.push({ name: name, placeId: placeId });
+  }
+  console.log('locations:', locations);
+
+})
+
+console.log('locations:', locations);
 function qrscanner(){
     console.log("clicked");
     window.location.href = "./qrscanner";
 }
 
-function displaymap(locations){
-    this.retrieveData(locations);
+function displaymap(){
+    console.log("clicked");
     window.location.href = "./displaymap";
     displayAll()
 }
@@ -17,7 +30,7 @@ function goBack( e ){
 
 // General Map Setup
 function initMap() {
-    map = new google.maps.Map(document.getElementById('map'), {
+    var map = new google.maps.Map(document.getElementById('map'), {
         center: {lat: 52.219841, lng: 6.896377},
         zoom: 16
     });
@@ -33,27 +46,12 @@ function displayAll(){
 }
 
 function displayStores(){
-    $.ajax({
-        method: "GET",
-        url: "/api/mastersheet",
-        dataType: "json",
-    }).fail(function(err){
-        console.error("Mastersheet call failed.", err)
-    }).always(function(){
-        console.info("Processing mastersheet call.")
-    }).done(function(data){
-        generateDisplays(data);
-    })
-}
-
-function generateDisplays(locations){
-    for(var i = 0 ; i < locations.length;  i++) {
-        var placeIdInfo = locations[i].placeId;
+    for (var i = 0 ; i < locations.length;  i++) {
+        var placeIdInfo = locations[i][4];
 
         var infowindow = new google.maps.InfoWindow();
         var service = new google.maps.places.PlacesService(map);
     
-        console.log("we MAKIN A NEW ONE", locations[i].name)
         service.getDetails(
           {
          placeId: placeIdInfo
@@ -67,7 +65,6 @@ function generateDisplays(locations){
                   position: place.geometry.location,
                        icon: iconBase + 'icon-gallery.png'
                 });
-                console.log("ping me bitch");
               google.maps.event.addListener(marker, 'click', function()
                 {
                 infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + 'Place ID: ' + place.place_id	+ '<br>' + '<br>'	+ '<b>Adress:</b>' + '<br>'	+ place.formatted_address	+ '</div>'+ '<br>' + 'Opening Times:' + '<br>' + place.opening_hours.weekday_text[0] + '<br>' + place.opening_hours.weekday_text[1]	+ '<br>' + place.opening_hours.weekday_text[2]	+ '<br>' + place.opening_hours.weekday_text[3]	+ '<br>' + place.opening_hours.weekday_text[4] + '<br>' + place.opening_hours.weekday_text[5] + '<br>' + place.opening_hours.weekday_text[6]);
@@ -90,6 +87,28 @@ function displayRestaurants(){
         console.log(data.results);
     }
     request.send();
+
+
+    service.getDetails(
+        {
+       latitude
+        }, function(place, status) {
+          if (status === google.maps.places.PlacesServiceStatus.OK)
+          {
+         var iconBase = 'http://retroactivesolutions.com/premadeMapForPool2/';
+               var marker = new google.maps.Marker(
+              {
+                map: map,
+                position: place.geometry.location,
+                     icon: iconBase + 'icon-gallery.png'
+              });
+            google.maps.event.addListener(marker, 'click', function()
+              {
+              infowindow.setContent('<div><strong>' + place.name + '</strong><br>' + 'Place ID: ' + place.place_id	+ '<br>' + '<br>'	+ '<b>Adress:</b>' + '<br>'	+ place.formatted_address	+ '</div>'+ '<br>' + 'Opening Times:' + '<br>' + place.opening_hours.weekday_text[0] + '<br>' + place.opening_hours.weekday_text[1]	+ '<br>' + place.opening_hours.weekday_text[2]	+ '<br>' + place.opening_hours.weekday_text[3]	+ '<br>' + place.opening_hours.weekday_text[4] + '<br>' + place.opening_hours.weekday_text[5] + '<br>' + place.opening_hours.weekday_text[6]);
+              infowindow.open(map, this);
+                  });
+                }
+        });
 }
 
 

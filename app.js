07f3b9012792b38522eef12167afead854a523f4
Jordan <jordan@familyque.st>
2018-07-04 20:48:52 +0200

    Database API functional, retrieving locations

--- app.js
+++ app.js
@@ -1,14 +1,10 @@
-// Firebase App is always required and must be first
-// var firebase = require("firebase/app");
-// require("firebase/database");
 var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
-
-
+var router = express.Router();
+var db = require('./initializesdk.js');
 var indexRouter = require('./routes/index');
 var qrscannerRouter = require('./routes/qrscanner');
 var dismapRouter = require('./routes/displaymap');
@@ -20,7 +16,6 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 app.set('port', process.env.PORT || 3071);
 
-
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -30,6 +25,24 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/qrscanner', qrscannerRouter);
 app.use('/displaymap', dismapRouter);
+
+router.route("/api/mastersheet").get(function(req, res) {
+  var locations = [];
+
+  db.ref("masterSheet").once('value').then(function(snapshot){
+
+    var allItems = snapshot.val();
+    for(let i = 1; i < allItems.length; i++){
+        var placeId = allItems[i][4];
+        var name = allItems[i][1];
+        locations.push({ name: name, placeId: placeId });
+    }
+    console.log("locs:", locations);
+    res.send(locations);
+    })
+})
+
+app.use("/", router);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -41,7 +54,6 @@ app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
-
   // render the error page
   res.status(err.status || 500);
   res.render('error');
